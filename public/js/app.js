a7061b161f0ab34c02a4e5ff2cc3a40c3f16c539
Jacob Talley <jacob.talley@hotmail.com>
2015-10-30 17:13:00 -0600

    got router working!!

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('devvit', ['ui-router']);
+var app = angular.module('devvit', ['ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider){
 	$urlRouterProvider.otherwise('/home/web')
