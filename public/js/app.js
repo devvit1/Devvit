470d1f263aa7c97ce4c597de3945c664bff5a8fc
Jacob Talley <jacob.talley@hotmail.com>
2015-10-31 12:42:43 -0600

    creating base template and routes

--- public/js/app.js
+++ public/js/app.js
@@ -6,12 +6,14 @@ app.config(function($stateProvider, $urlRouterProvider){
 	$stateProvider
 		.state('devvit', {
 			url: '/home',
-			templateUrl: '../templates/projectView.html'
+			templateUrl: '../templates/homeView.html'
 		})
 			.state('devvit.web', {
 				url: '/web',
 				templateUrl:'../templates/projectView.html'
 			})
+		
+			
 		.state('profile', {
 			url:'/profile/:user_id',
 			templateUrl:'../templates/profile.html'
