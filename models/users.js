0a3321befe7a8e11280537d1770d1540783dc52d
William Mech <Billy@Williams-MacBook-Air.local>
2015-11-02 11:45:26 -0700

    added required fields

--- models/users.js
+++ models/users.js
@@ -1,17 +1,17 @@
 var mongoose = require('mongoose');
 
-var UserSchema = new mongoose.Schema({
+var UsersSchema = new mongoose.Schema({
 	basicInfo: {
 		firstName: { type: String, required: true },
 		lastName: { type: String, required: true },
 		userName: { type: String, required: true },
-		location: { type: String, required: true },
 		email: { type: String, required: true },
+		location: String,
 		gitHubUrl: String,
 		linkedinUrl: String
 	},
-	pendingApprovals: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Projects'}],
-	activeGroups: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Projects'}],
+	pendingApprovals: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Projects' }],
+	activeGroups: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Projects' }],
 	inactiveGroups: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Projects' }],
 	pastGroups: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Projects' }],
 	messages: [{ type: String }],
