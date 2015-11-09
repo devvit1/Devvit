eeb74a68d41ffae4e9d352ea6324f12c62f79a76
William Mech <Billy@Williams-MacBook-Air.local>
2015-11-09 16:18:58 -0700

    apply web

--- public/js/profile/controllers/pseudoUserCtrl.js
+++ public/js/profile/controllers/pseudoUserCtrl.js
@@ -4,13 +4,14 @@ angular.module('devvit').controller('pseudoCtrl', function ($scope, $rootScope, 
 	
 	
 		var user1 = {
-			//antonio
-			_id: "563bdf93f06dd090a073dc13"
-		}
+			//jacob
+			_id: "563d35c90e74db4d1e6c37b4"
+		};
+
 		
 		//jacob
 		var user2 = {
-			_id: "563bdf9df06dd090a073dc14"
+			_id: "563d360446a0f06b1ea87b84"
 		};
 
 		//joe
@@ -29,6 +30,7 @@ angular.module('devvit').controller('pseudoCtrl', function ($scope, $rootScope, 
 
 
 	$rootScope.profile = user4;
+
 
 
 
